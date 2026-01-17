1ffe7068cc335b887201455a19078085962341df
ethio-man <natymiskir@gmail.com>
2026-01-17 07:22:45 -0300

    Operator page created

--- Forms/Operator/MeetingForm.Designer.cs
+++ Forms/Operator/MeetingForm.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MeetingForm";
+            tabOperator = new TabControl();
+            tabMetting = new TabPage();
+            tabAgenda = new TabPage();
+            tabRecording = new TabPage();
+            tabAssignments = new TabPage();
+            tabOperator.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabOperator
+            // 
+            tabOperator.Controls.Add(tabMetting);
+            tabOperator.Controls.Add(tabAgenda);
+            tabOperator.Controls.Add(tabRecording);
+            tabOperator.Controls.Add(tabAssignments);
+            tabOperator.Location = new Point(3, 2);
+            tabOperator.Name = "tabOperator";
+            tabOperator.SelectedIndex = 0;
+            tabOperator.Size = new Size(801, 453);
+            tabOperator.TabIndex = 0;
+            // 
+            // tabMetting
+            // 
+            tabMetting.Location = new Point(4, 24);
+            tabMetting.Name = "tabMetting";
+            tabMetting.Padding = new Padding(3);
+            tabMetting.Size = new Size(793, 425);
+            tabMetting.TabIndex = 0;
+            tabMetting.Text = "Meetings";
+            tabMetting.UseVisualStyleBackColor = true;
+            // 
+            // tabAgenda
+            // 
+            tabAgenda.Location = new Point(4, 24);
+            tabAgenda.Name = "tabAgenda";
+            tabAgenda.Padding = new Padding(3);
+            tabAgenda.Size = new Size(793, 425);
+            tabAgenda.TabIndex = 1;
+            tabAgenda.Text = "Agendas";
+            tabAgenda.UseVisualStyleBackColor = true;
+            // 
+            // tabRecording
+            // 
+            tabRecording.Location = new Point(4, 24);
+            tabRecording.Name = "tabRecording";
+            tabRecording.Size = new Size(793, 425);
+            tabRecording.TabIndex = 2;
+            tabRecording.Text = "Recordings";
+            tabRecording.UseVisualStyleBackColor = true;
+            // 
+            // tabAssignments
+            // 
+            tabAssignments.Location = new Point(4, 24);
+            tabAssignments.Name = "tabAssignments";
+            tabAssignments.Size = new Size(793, 425);
+            tabAssignments.TabIndex = 3;
+            tabAssignments.Text = "Assignments";
+            tabAssignments.UseVisualStyleBackColor = true;
+            // 
+            // MeetingForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabOperator);
+            Name = "MeetingForm";
+            Text = "MeetingForm";
+            tabOperator.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabOperator;
+        private TabPage tabMetting;
+        private TabPage tabAgenda;
+        private TabPage tabRecording;
+        private TabPage tabAssignments;
     }
 }
