27f7a4ddd9cfab799dbf83e59fb9efe922adc478
Daniel-1961 <danieldata36@gmail.com>
2026-01-19 01:17:46 +0300

    Added Admin Dashboard, Updated Login Form

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -37,6 +37,7 @@
             lblMessage = new Label();
             picLogo = new PictureBox();
             picFlag = new PictureBox();
+            lblTitle = new Label();
             ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picFlag).BeginInit();
             SuspendLayout();
@@ -44,17 +45,16 @@
             // lblUsername
             // 
             lblUsername.AutoSize = true;
-            lblUsername.Location = new Point(71, 155);
+            lblUsername.Location = new Point(297, 230);
             lblUsername.Name = "lblUsername";
             lblUsername.Size = new Size(63, 25);
             lblUsername.TabIndex = 0;
             lblUsername.Text = "Name:";
-           
             // 
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(71, 239);
+            lblPassword.Location = new Point(297, 291);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new Size(91, 25);
             lblPassword.TabIndex = 1;
@@ -62,14 +62,14 @@
             // 
             // textUsername
             // 
-            textUsername.Location = new Point(190, 149);
+            textUsername.Location = new Point(426, 224);
             textUsername.Name = "textUsername";
             textUsername.Size = new Size(240, 31);
             textUsername.TabIndex = 2;
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(190, 233);
+            txtPassword.Location = new Point(426, 285);
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(240, 31);
             txtPassword.TabIndex = 3;
@@ -77,30 +77,31 @@
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(64, 368);
+            btnLogin.Location = new Point(272, 447);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(98, 39);
             btnLogin.TabIndex = 4;
             btnLogin.Text = "Login";
             btnLogin.UseVisualStyleBackColor = true;
-           
             // 
             // lblMessage
             // 
             lblMessage.AutoSize = true;
             lblMessage.ForeColor = Color.Red;
-            lblMessage.Location = new Point(369, 213);
+            lblMessage.Location = new Point(384, 213);
             lblMessage.Name = "lblMessage";
             lblMessage.Size = new Size(0, 25);
             lblMessage.TabIndex = 5;
             // 
             // picLogo
             // 
+            picLogo.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            picLogo.BackColor = Color.Transparent;
             picLogo.BorderStyle = BorderStyle.FixedSingle;
             picLogo.Image = (Image)resources.GetObject("picLogo.Image");
-            picLogo.Location = new Point(578, 12);
+            picLogo.Location = new Point(714, -2);
             picLogo.Name = "picLogo";
-            picLogo.Size = new Size(200, 75);
+            picLogo.Size = new Size(200, 111);
             picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
             picLogo.TabIndex = 6;
             picLogo.TabStop = false;
@@ -108,18 +109,29 @@
             // picFlag
             // 
             picFlag.Image = (Image)resources.GetObject("picFlag.Image");
-            picFlag.Location = new Point(49, 12);
+            picFlag.Location = new Point(1, -2);
             picFlag.Name = "picFlag";
-            picFlag.Size = new Size(150, 88);
+            picFlag.Size = new Size(150, 111);
             picFlag.SizeMode = PictureBoxSizeMode.StretchImage;
             picFlag.TabIndex = 7;
             picFlag.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Location = new Point(293, 96);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(415, 74);
+            lblTitle.TabIndex = 8;
+            lblTitle.Text = "Mini-DARMAS";
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(916, 639);
+            Controls.Add(lblTitle);
             Controls.Add(picFlag);
             Controls.Add(picLogo);
             Controls.Add(lblMessage);
@@ -128,12 +140,8 @@
             Controls.Add(textUsername);
             Controls.Add(lblPassword);
             Controls.Add(lblUsername);
-            FormBorderStyle = FormBorderStyle.None;
-            MaximizeBox = false;
-            MinimizeBox = false;
             Name = "LoginForm";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Mini-DARMAS Login";
+            Text = "Login";
             ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
             ((System.ComponentModel.ISupportInitialize)picFlag).EndInit();
             ResumeLayout(false);
@@ -150,5 +158,6 @@
         private Label lblMessage;
         private PictureBox picLogo;
         private PictureBox picFlag;
+        private Label lblTitle;
     }
 }
