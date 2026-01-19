4287bf1f186a2e6a47d796e50f8bff156a5e2690
ethio-man <natymiskir@gmail.com>
2026-01-19 11:17:52 -0300

    meetingDao is added

--- obj/Debug/net8.0-windows/Mini-DARMAS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Mini-DARMAS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini-DARMAS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7388302b5e113956f8d17733821d84d89e939998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc1b1239072395c64e23282326f7798be5b3d8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini-DARMAS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini-DARMAS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
